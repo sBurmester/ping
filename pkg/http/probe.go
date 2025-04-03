eb2198caf526abc9259c74d45bceb8718b2071ad
sburmester <sebastian.burmester@mail.de>
2025-04-03 23:13:09 +0200

    adds probe for kubernetes

---
+++ pkg/http/probe.go
@@ -0,0 +1,28 @@
+package http
+
+import (
+	"net/http"
+	"sync/atomic"
+)
+
+type Probe struct {
+	up atomic.Bool
+}
+
+func (p *Probe) Up() {
+	p.up.Store(true)
+}
+
+func (p *Probe) Down() {
+	p.up.Store(false)
+}
+
+func (p *Probe) ServeHTTP(wr http.ResponseWriter, r *http.Request) {
+	if p.up.Load() {
+		wr.WriteHeader(http.StatusNoContent)
+		return // prevent superfluous call of response.WriteHeader with StatusServiceUnavailable
+	}
+	wr.WriteHeader(http.StatusServiceUnavailable)
+}
+
+var Live, Ready = &Probe{}, &Probe{}
