9558df10a46f4e9560cfc19d99fe102ed59d56bf
sburmester <sebastian.burmester@mail.de>
2025-05-17 20:35:53 +0200

    adds default metrics and automemlimit/automaxprocs - adds graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/sburmester/ping/pkg/http"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+
+	_ "github.com/KimMachineGun/automemlimit" // for side effects
+	_ "go.uber.org/automaxprocs"              // for side effects
 )
 
 func main() {
@@ -16,10 +32,89 @@ func main() {
 }
 
 func run() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		ch := make(chan os.Signal, 1)
+
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		defer signal.Stop(ch)
+
+		select {
+		case <-ch:
+			cancel()
+		case <-ctx.Done():
+			return
+		}
+	}()
+
 	slog.Info("starting ping server")
-	err := http.StartServer()
+
+	shutdown, err := installTelemetry()
+
+	if err != nil {
+		return fmt.Errorf("cannot install telemetry: %w", err)
+	}
+
+	defer func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		if err := shutdown(ctx); err != nil {
+			slog.Default().Error("cannot shutdown telemetry", err.Error())
+		}
+	}()
+
+	err = http.StartServer()
 	if err != nil {
 		slog.Default().Error("error starting server", err)
 	}
 	return err
+}
+
+func installTelemetry() (func(context.Context) error, error) {
+	r, err := resource.New(
+		context.Background(),
+
+		resource.WithTelemetrySDK(),
+
+		resource.WithSchemaURL(semconv.SchemaURL),
+		resource.WithAttributes(
+			semconv.ServiceName("ping-server"),
+		),
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot create resource: %w", err)
+	}
+
+	propagator := propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+
+	otel.SetTextMapPropagator(propagator)
+
+	metricReader, err := prometheus.New()
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot create metric reader: %w", err)
+	}
+
+	meterProvider := metric.NewMeterProvider(
+		metric.WithResource(r),
+		metric.WithReader(metricReader),
+	)
+
+	otel.SetMeterProvider(meterProvider)
+
+	shutdown := func(ctx context.Context) error {
+		return errors.Join(
+			meterProvider.ForceFlush(ctx),
+			meterProvider.Shutdown(ctx),
+		)
+	}
+
+	return shutdown, nil
 }
--- pkg/http/serve.go
+++ pkg/http/serve.go
@@ -3,31 +3,74 @@ package http
 import (
 	"net/http"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/metric"
 )
+
+const name = "go.opentelemetry.io/otel/homelab/ping"
+
+var (
+	tracer  = otel.Tracer(name)
+	meter   = otel.Meter(name)
+	pingCnt metric.Int64Counter
+)
+
+func init() {
+	var err error
+	pingCnt, err = meter.Int64Counter("ping.request.count",
+		metric.WithDescription("The number of ping requests"),
+		metric.WithUnit("{ping}"))
+	if err != nil {
+		panic(err)
+	}
+}
 
 // handlerPing handles the /ping endpoint, responding with "pong".
 func handlerPing(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("pong"))
+	ctx, span := tracer.Start(r.Context(), "roll")
+	defer span.End()
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	pingCnt.Add(ctx, 1)
+
+	w.Header().Set("Content-Type", "text/plain")
+	_, err := w.Write([]byte("pong"))
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func addHandler(router *http.ServeMux) {
-	router.HandleFunc("/ping", handlerPing)
-	router.Handle("/livez", Live)
-	router.Handle("/readyz", Ready)
+	router.HandleFunc("GET /ping", handlerPing)
+	router.Handle("GET /livez", Live)
+	router.Handle("GET /readyz", Ready)
+	router.Handle("GET /metrics", promhttp.Handler())
 
 }
 
 func StartServer() error {
-	router := http.NewServeMux()
 	Live.Up()
+	router := http.NewServeMux()
 	addHandler(router)
 
-	Ready.Up()
 	server := &http.Server{
 		Addr:              ":8080",
 		Handler:           router,
 		ReadHeaderTimeout: 5 * time.Second,
 	}
+
+	// Add more timeouts for better security and reliability
+	server.WriteTimeout = 10 * time.Second
+	server.IdleTimeout = 60 * time.Second
+
+	Ready.Up()
 
 	return server.ListenAndServe()
 }
